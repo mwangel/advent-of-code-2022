70c7b89220676dee47efd753da8b66eb768eaac4
Martin Wangel <martin.wangel@gmail.com>
2022-12-06 21:57:48 +0100

    Added javadoc to day 6

--- 06/a202205.groovy
+++ 06/a202205.groovy
@@ -1,16 +1,21 @@
 String data = ( "data06.txt" as File ).text
 
+/** True if all characters in s are different. */
 def allDifferentAsSet( String s ) {
     int i = s.size()
     return i == (s.toSet()).size()
 }
 
+/** Find an index in s where there are n different characters. */
 def findFirstDifferent( String s, int n ) {
     for( int i = 0; i < s.size() - n + 1; i++ ) {
         def sub = s.substring( i, i + n )
         if( allDifferentAsSet( sub ) ) return i + n
     }
-    return -1
+
+    // A return value of -1 means that there is no place in s
+    //  where all characters are different.
+    return -1.
 }
 
 def result = findFirstDifferent( data, 4 )
